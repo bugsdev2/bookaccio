16f4d80fa7d8b5fde4659ca9a55b0f07daf37aaa
Aadi <adithyamanikumar@proton.me>
2024-11-12 23:49:55 +0530

    Added helper functions to process durationa and interjections

---
+++ helpers/processDuration.ts
@@ -0,0 +1,9 @@
+export const processDuration = (startDate: number, endDate: number) => {
+    const days = Math.round((endDate - startDate) / (1000 * 60 * 60 * 24));
+    if (days === 0) {
+        return 'less than a day.';
+    } else if (days === 1) {
+        return '1 day.';
+    }
+    return `${days} days.`;
+};
---
+++ helpers/processInterjections.ts
@@ -0,0 +1,5 @@
+export const processInterjections = () => {
+    const interjections = ['Good Job!', 'Hurray!', 'Yay!', 'Nice work!', 'Awesome!', 'Bravo!', 'Cheers, mate!', 'Excellent!', 'Fabulous!', 'Fantastic!', 'Yippee!', 'Woo-hoo!', 'Swell!', 'Marvelous!'];
+
+    return interjections[Math.floor(Math.random() * interjections.length)];
+};
