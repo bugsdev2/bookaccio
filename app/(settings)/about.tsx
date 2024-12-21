633fa20efd0aa2709cb6da0305da5f50a16989fc
Aadi <adithyamanikumar@proton.me>
2024-12-21 17:19:20 +0530

    Displaying version number in the about page

--- app/(settings)/about.tsx
+++ app/(settings)/about.tsx
@@ -5,150 +5,161 @@ import { useDarkModeContext } from '@/providers/themeProvider';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useAccentColorContext } from '@/providers/accentColorProvider';
 import * as WebBrowser from 'expo-web-browser';
+import { versionNum } from '@/constants/versionNum';
 
 const About = () => {
-    const [isDarkMode, setIsDarkMode] = useDarkModeContext();
+  const [isDarkMode, setIsDarkMode] = useDarkModeContext();
 
-    const [accentColor, setAccentColor] = useAccentColorContext();
+  const [accentColor, setAccentColor] = useAccentColorContext();
 
-    function handleLink(url: string) {
-        WebBrowser.openBrowserAsync(url);
-    }
+  function handleLink(url: string) {
+    WebBrowser.openBrowserAsync(url);
+  }
 
-    const Link = ({ text, url }: { text: string; url: string }) => {
-        return (
-            <Text
-                onPress={() => handleLink(url)}
-                style={[styles.linkText, { color: accentColor }]}
-            >
-                {` ${text} `}
-            </Text>
-        );
-    };
-
+  const Link = ({ text, url }: { text: string; url: string }) => {
     return (
-        <ScrollView
-            style={[styles.container, { backgroundColor: isDarkMode ? Colors.dark : Colors.light }]}
-            contentContainerStyle={styles.contentContainer}
-        >
-            <SafeAreaView>
-                <View style={{ width: '100%' }}>
-                    <Text style={[styles.title, { color: accentColor }]}>About</Text>
-                </View>
-                <View style={{ width: '97%' }}>
-                    <Text style={[styles.text, { color: isDarkMode ? Colors.white : Colors.dark }]}>Bookaccio is your ultimate book tracking companion. Add, organize, and manage your reading list with ease using data from the Google Books API. Track reading progress, update book details, and export your library with one tap! (Well... Maybe a few taps...)</Text>
-                    <Text style={[styles.text, { color: isDarkMode ? Colors.white : Colors.dark }]}>Bookaccio is not just free to use, it is also ad-free and also opensource.</Text>
-                </View>
-                <View>
-                    <Text style={[styles.title, { color: accentColor }]}>What's with the name?</Text>
-                    <Text style={[styles.text, { color: isDarkMode ? Colors.white : Colors.dark }]}>The name "Bookaccio" can be pronounced in two different ways: </Text>
-                    <Text style={[styles.textStrong, { color: isDarkMode ? Colors.white : Colors.dark }]}>
-                        Bu-kah-ch(ee)oh <Text style={[styles.text, { color: isDarkMode ? Colors.white : Colors.dark }]}>(IPA: /bʊˈkɑːtʃ(i)oʊ/)</Text>
-                    </Text>
-                    <Text style={[styles.text, { color: isDarkMode ? Colors.white : Colors.dark }]}>
-                        This app is inspired by
-                        <Link
-                            text="Dante Book Tracker"
-                            url="https://github.com/shockbytes/DanteX"
-                        />
-                        app created by
-                        <Link
-                            text="Martin Macheiner."
-                            url="https://github.com/shockbytes"
-                        />
-                        Since there have been no recent developments in that app, I thought of making my own. Of course, lots of features are yet to be added.
-                    </Text>
-                    <Text style={[styles.text, { color: isDarkMode ? Colors.white : Colors.dark }]}>
-                        The name 'Bookaccio' is a nod to one of Italy’s most influential writers
-                        <Link
-                            text="Giovanni Boccaccio"
-                            url="https://en.wikipedia.org/wiki/Giovanni_Boccaccio"
-                        />
-                        who was a contemporary of
-                        <Link
-                            text="Dante Alighieri."
-                            url="https://en.wikipedia.org/wiki/Dante_Alighieri"
-                        />
-                        While the Dante Book Tracker app made no explicit connections to Dante Alighieri, I enjoy thinking of this small connection between my "Booccaccio" and Martin's "Dante"
-                    </Text>
-                    <Text style={[styles.textStrong, { color: isDarkMode ? Colors.white : Colors.dark }]}>
-                        Book-ack-ee-oh <Text style={[styles.text, { color: isDarkMode ? Colors.white : Colors.dark }]}>(IPA: /bʊ'kækioʊ/)</Text>
-                    </Text>
-                    <Text style={[styles.text, { color: isDarkMode ? Colors.white : Colors.dark }]}>This pronunciation is most probably the one you're likely to think of when you see the name for the first time.</Text>
-                    <Text style={[styles.text, { color: isDarkMode ? Colors.white : Colors.dark }]}>
-                        It is inspired by the summoning spell
-                        <Link
-                            text='"Accio"'
-                            url="https://harrypotter.fandom.com/wiki/Summoning_Charm"
-                        />
-                        from the Harry Potter series, used to magically call objects to you just like how the app helps you “summon” books and organize them.
-                    </Text>
-                </View>
-                <View>
-                    <Text style={[styles.title, { color: accentColor }]}>Do you like using Bookaccio?</Text>
-                    <Text style={[styles.text, { color: isDarkMode ? Colors.white : Colors.dark }]}>I'm a freelance web and app developer. I'm always working on some project or the other whenever I'm free. And all the stuff I've created so far are not only free to use and ad-free but also opensource.</Text>
-                    <Text style={[styles.text, { color: isDarkMode ? Colors.white : Colors.dark }]}>Do you like using my app? Would you like to contrinute a little something so that I can create more free and opensource apps? You can</Text>
-                </View>
-                <View style={{ flexDirection: 'row', gap: 20, justifyContent: 'center' }}>
-                    <Pressable style={styles.btn}>
-                        <Link
-                            text="Buy me a coffee"
-                            url="https://buymeacoffee.com/bugsdev2"
-                        />
-                    </Pressable>
-                    <Pressable style={styles.btn}>
-                        <Link
-                            text="Hire me"
-                            url="https://bugsdev2.github.io/portfolio/"
-                        />
-                    </Pressable>
-                </View>
-            </SafeAreaView>
-        </ScrollView>
+      <Text
+        onPress={() => handleLink(url)}
+        style={[styles.linkText, { color: accentColor }]}
+      >
+        {` ${text} `}
+      </Text>
     );
+  };
+
+  return (
+    <ScrollView
+      style={[styles.container, { backgroundColor: isDarkMode ? Colors.dark : Colors.light }]}
+      contentContainerStyle={styles.contentContainer}
+    >
+      <SafeAreaView>
+        <View style={{ width: '100%' }}>
+          <Text style={[styles.title, { color: accentColor }]}>About</Text>
+        </View>
+        <View>
+          <Text style={[styles.versionNumber, { color: isDarkMode ? Colors.light : Colors.dark }]}>Bookaccio v{versionNum}</Text>
+        </View>
+        <View style={{ width: '97%' }}>
+          <Text style={[styles.text, { color: isDarkMode ? Colors.white : Colors.dark }]}>Bookaccio is your ultimate book tracking companion. Add, organize, and manage your reading list with ease using data from the Google Books API. Track reading progress, update book details, and export your library with one tap! (Well... Maybe a few taps...)</Text>
+          <Text style={[styles.text, { color: isDarkMode ? Colors.white : Colors.dark }]}>Bookaccio is not just free to use, it is also ad-free and also opensource.</Text>
+        </View>
+        <View>
+          <Text style={[styles.title, { color: accentColor }]}>What's with the name?</Text>
+          <Text style={[styles.text, { color: isDarkMode ? Colors.white : Colors.dark }]}>The name "Bookaccio" can be pronounced in two different ways: </Text>
+          <Text style={[styles.textStrong, { color: isDarkMode ? Colors.white : Colors.dark }]}>
+            Bu-kah-ch(ee)oh <Text style={[styles.text, { color: isDarkMode ? Colors.white : Colors.dark }]}>(IPA: /bʊˈkɑːtʃ(i)oʊ/)</Text>
+          </Text>
+          <Text style={[styles.text, { color: isDarkMode ? Colors.white : Colors.dark }]}>
+            This app is inspired by
+            <Link
+              text="Dante Book Tracker"
+              url="https://github.com/shockbytes/DanteX"
+            />
+            app created by
+            <Link
+              text="Martin Macheiner."
+              url="https://github.com/shockbytes"
+            />
+            Since there have been no recent developments in that app, I thought of making my own. Of course, lots of features are yet to be added.
+          </Text>
+          <Text style={[styles.text, { color: isDarkMode ? Colors.white : Colors.dark }]}>
+            The name 'Bookaccio' is a nod to one of Italy’s most influential writers
+            <Link
+              text="Giovanni Boccaccio"
+              url="https://en.wikipedia.org/wiki/Giovanni_Boccaccio"
+            />
+            who was a contemporary of
+            <Link
+              text="Dante Alighieri."
+              url="https://en.wikipedia.org/wiki/Dante_Alighieri"
+            />
+            While the Dante Book Tracker app made no explicit connections to Dante Alighieri, I enjoy thinking of this small connection between my "Booccaccio" and Martin's "Dante"
+          </Text>
+          <Text style={[styles.textStrong, { color: isDarkMode ? Colors.white : Colors.dark }]}>
+            Book-ack-ee-oh <Text style={[styles.text, { color: isDarkMode ? Colors.white : Colors.dark }]}>(IPA: /bʊ'kækioʊ/)</Text>
+          </Text>
+          <Text style={[styles.text, { color: isDarkMode ? Colors.white : Colors.dark }]}>This pronunciation is most probably the one you're likely to think of when you see the name for the first time.</Text>
+          <Text style={[styles.text, { color: isDarkMode ? Colors.white : Colors.dark }]}>
+            It is inspired by the summoning spell
+            <Link
+              text='"Accio"'
+              url="https://harrypotter.fandom.com/wiki/Summoning_Charm"
+            />
+            from the Harry Potter series, used to magically call objects to you just like how the app helps you “summon” books and organize them.
+          </Text>
+        </View>
+        <View>
+          <Text style={[styles.title, { color: accentColor }]}>Do you like using Bookaccio?</Text>
+          <Text style={[styles.text, { color: isDarkMode ? Colors.white : Colors.dark }]}>I'm a freelance web and app developer. I'm always working on some project or the other whenever I'm free. And all the stuff I've created so far are not only free to use and ad-free but also opensource.</Text>
+          <Text style={[styles.text, { color: isDarkMode ? Colors.white : Colors.dark }]}>Do you like using my app? Would you like to contrinute a little something so that I can create more free and opensource apps? You can</Text>
+        </View>
+        <View style={{ flexDirection: 'row', gap: 20, justifyContent: 'center' }}>
+          <Pressable style={styles.btn}>
+            <Link
+              text="Buy me a coffee"
+              url="https://buymeacoffee.com/bugsdev2"
+            />
+          </Pressable>
+          <Pressable style={styles.btn}>
+            <Link
+              text="Hire me"
+              url="https://bugsdev2.github.io/portfolio/"
+            />
+          </Pressable>
+        </View>
+      </SafeAreaView>
+    </ScrollView>
+  );
 };
 
 export default About;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
+  container: {
+    flex: 1,
+  },
 
-    contentContainer: {
-        padding: 15,
-        paddingBottom: 25,
-    },
+  versionNumber: {
+    textAlign: 'center',
+    fontSize: 18,
+    fontFamily: 'MontB',
+    paddingBottom: 10,
+  },
 
-    title: {
-        fontSize: 28,
-        textAlign: 'center',
-        fontFamily: 'MontB',
-        marginBottom: 10,
-    },
+  contentContainer: {
+    padding: 15,
+    paddingBottom: 25,
+  },
 
-    text: {
-        fontFamily: 'MontR',
-        fontSize: 15,
-        lineHeight: 30,
-        marginBottom: 15,
-    },
+  title: {
+    fontSize: 28,
+    textAlign: 'center',
+    fontFamily: 'MontB',
+    marginBottom: 10,
+  },
 
-    textStrong: {
-        fontSize: 18,
-        fontFamily: 'MontB',
-    },
+  text: {
+    fontFamily: 'MontR',
+    fontSize: 15,
+    lineHeight: 30,
+    marginBottom: 15,
+  },
 
-    linkText: {
-        fontWeight: 'bold',
-    },
+  textStrong: {
+    fontSize: 18,
+    fontFamily: 'MontB',
+  },
 
-    btn: {
-        paddingHorizontal: 25,
-        paddingVertical: 10,
-        borderWidth: 1,
-        alignSelf: 'center',
-        backgroundColor: Colors.light,
-        borderRadius: 20,
-    },
+  linkText: {
+    fontWeight: 'bold',
+  },
+
+  btn: {
+    paddingHorizontal: 25,
+    paddingVertical: 10,
+    borderWidth: 1,
+    alignSelf: 'center',
+    backgroundColor: Colors.light,
+    borderRadius: 20,
+  },
 });
--- app/(settings)/settings.tsx
+++ app/(settings)/settings.tsx
@@ -16,188 +16,184 @@ import { storeBooks } from '@/helpers/storeBooks';
 import * as WebBrowser from 'expo-web-browser';
 
 const Settings = () => {
-    const [isDarkMode, setIsDarkMode] = useDarkModeContext();
+  const [isDarkMode, setIsDarkMode] = useDarkModeContext();
 
-    const [accentColor, setAccentColor] = useAccentColorContext();
+  const [accentColor, setAccentColor] = useAccentColorContext();
 
-    const [fullBookList, setFullBookList] = useFullBookListContext();
+  const [fullBookList, setFullBookList] = useFullBookListContext();
 
-    function handleLink(url: string) {
-        WebBrowser.openBrowserAsync(url);
+  function handleLink(url: string) {
+    WebBrowser.openBrowserAsync(url);
+  }
+
+  const handleImport = async () => {
+    let result = await DocumentPicker.getDocumentAsync({});
+    if (!result.canceled) {
+      try {
+        const data = await FileSystem.readAsStringAsync(result.assets[0].uri);
+        const fileData: Book[] = JSON.parse(await data);
+        if (fileData[0].id && fileData[0].title) {
+          Alert.alert('Warning!', 'This will remove all existing books from your library and add the new list', [
+            {
+              text: 'Okay',
+              onPress: () => (setFullBookList([...fileData]), storeBooks(fileData), Alert.alert('Success', 'File successfully imported')),
+            },
+            {
+              text: 'Cancel',
+            },
+          ]);
+        } else {
+          throw new Error('You might have provided a different file. You can use only a bookaccio export file');
+        }
+      } catch (err) {
+        Alert.alert('Error', 'You might have provided a different file. You can use only a bookaccio export file');
+        console.log(err);
+      }
+    } else {
+      Alert.alert('Canceled', 'You have canceled the import');
     }
+  };
 
-    const handleImport = async () => {
-        let result = await DocumentPicker.getDocumentAsync({});
-        if (!result.canceled) {
-            try {
-                const data = await FileSystem.readAsStringAsync(result.assets[0].uri);
-                const fileData: Book[] = JSON.parse(await data);
-                if (fileData[0].id && fileData[0].title) {
-                    Alert.alert('Warning!', 'This will remove all existing books from your library and add the new list', [
-                        {
-                            text: 'Okay',
-                            onPress: () => (setFullBookList([...fileData]), storeBooks(fileData), Alert.alert('Success', 'File successfully imported')),
-                        },
-                        {
-                            text: 'Cancel',
-                        },
-                    ]);
-                } else {
-                    throw new Error('You might have provided a different file. You can use only a bookaccio export file');
-                }
-            } catch (err) {
-                Alert.alert('Error', 'You might have provided a different file. You can use only a bookaccio export file');
-                console.log(err);
-            }
-        } else {
-            Alert.alert('Canceled', 'You have canceled the import');
-        }
-    };
+  const handleExport = async () => {
+    const permissions = await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
+    if (permissions.granted) {
+      let dirUrl = permissions.directoryUri;
+      getBookList().then((data) => {
+        setFullBookList(data);
+      });
 
-    const handleExport = async () => {
-        const permissions = await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
-        if (permissions.granted) {
-            let dirUrl = permissions.directoryUri;
-            getBookList().then((data) => {
-                setFullBookList(data);
-            });
+      await FileSystem.StorageAccessFramework.createFileAsync(dirUrl, `Bookaccio_Export_${new Date().toDateString().split(' ').join('_')}`, 'application/json')
+        .then(async (fileUri) => {
+          await FileSystem.writeAsStringAsync(fileUri, JSON.stringify(fullBookList), { encoding: FileSystem.EncodingType.UTF8 });
+          Alert.alert(`Export file saved successfully`);
+        })
+        .catch((e) => {
+          Alert.alert(`Error saving file: ${e}`);
+          console.log(e);
+        });
+    } else {
+      Alert.alert('You must allow storage permission to save the file.');
+    }
+  };
 
-            await FileSystem.StorageAccessFramework.createFileAsync(dirUrl, `Bookaccio_Export_${new Date().toDateString().split(' ').join('_')}`, 'application/json')
-                .then(async (fileUri) => {
-                    await FileSystem.writeAsStringAsync(fileUri, JSON.stringify(fullBookList), { encoding: FileSystem.EncodingType.UTF8 });
-                    Alert.alert(`Export file saved successfully`);
-                })
-                .catch((e) => {
-                    Alert.alert(`Error saving file: ${e}`);
-                    console.log(e);
-                });
-        } else {
-            Alert.alert('You must allow storage permission to save the file.');
-        }
-    };
+  return (
+    <ScrollView
+      style={[{ backgroundColor: isDarkMode ? Colors.black : Colors.light }]}
+      contentContainerStyle={styles.contentContainer}
+    >
+      <SafeAreaView>
+        <View style={styles.headerContainer}>
+          <Text style={[styles.headerTitle, { color: accentColor }]}>Settings</Text>
+        </View>
+        <View style={{ gap: 20 }}>
+          <View style={styles.sectionContainer}>
+            <Text style={[styles.subheading, { color: isDarkMode ? Colors.light : Colors.dark }]}>Colors</Text>
+            <SettingItem
+              label="Accent Color"
+              data={accentColors}
+            />
+            <SettingItem
+              label="Theme"
+              data={theme}
+            />
+          </View>
+          <View style={styles.sectionContainer}>
+            <Text style={[styles.subheading, { color: isDarkMode ? Colors.light : Colors.dark }]}>Fonts</Text>
+            <SettingItem
+              label="Font"
+              data={fonts}
+            />
+          </View>
+          <View style={styles.sectionContainer}>
+            <Text style={[styles.subheading, { color: isDarkMode ? Colors.light : Colors.dark }]}>Import/Export</Text>
 
-    return (
-        <ScrollView
-            style={[styles.container, { backgroundColor: isDarkMode ? Colors.black : Colors.light }]}
-            contentContainerStyle={styles.contentContainer}
-        >
-            <SafeAreaView>
-                <View style={styles.headerContainer}>
-                    <Text style={[styles.headerTitle, { color: accentColor }]}>Settings</Text>
-                </View>
-                <View style={{ gap: 20 }}>
-                    <View style={styles.sectionContainer}>
-                        <Text style={[styles.subheading, { color: isDarkMode ? Colors.light : Colors.dark }]}>Colors</Text>
-                        <SettingItem
-                            label="Accent Color"
-                            data={accentColors}
-                        />
-                        <SettingItem
-                            label="Theme"
-                            data={theme}
-                        />
-                    </View>
-                    <View style={styles.sectionContainer}>
-                        <Text style={[styles.subheading, { color: isDarkMode ? Colors.light : Colors.dark }]}>Fonts</Text>
-                        <SettingItem
-                            label="Font"
-                            data={fonts}
-                        />
-                    </View>
-                    <View style={styles.sectionContainer}>
-                        <Text style={[styles.subheading, { color: isDarkMode ? Colors.light : Colors.dark }]}>Import/Export</Text>
-
-                        <View style={styles.btnContainer}>
-                            <TouchableOpacity
-                                style={[styles.btn, { backgroundColor: accentColor }]}
-                                onPress={handleImport}
-                            >
-                                <Text style={styles.text}>Import</Text>
-                            </TouchableOpacity>
-                            <TouchableOpacity
-                                style={[styles.btn, { backgroundColor: accentColor }]}
-                                onPress={handleExport}
-                            >
-                                <Text style={styles.text}>Export</Text>
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                    <View style={styles.sectionContainer}>
-                        <Text style={[styles.subheading, { color: isDarkMode ? Colors.light : Colors.dark }]}>Report a Bug</Text>
-                        <View style={{ flexDirection: 'column', gap: 15 }}>
-                            <TouchableOpacity
-                                onPress={() => handleLink('https://t.me/+2qtifdFNRv00Y2M9')}
-                                style={[styles.btn, { backgroundColor: accentColor }]}
-                            >
-                                <Text style={styles.text}>Join the Official Telegram Group</Text>
-                            </TouchableOpacity>
-                            <TouchableOpacity
-                                onPress={() => handleLink('https://github.com/bugsdev2/bookaccio/issues')}
-                                style={[styles.btn, { backgroundColor: accentColor }]}
-                            >
-                                <Text style={styles.text}>Create a New Issue via Github</Text>
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                </View>
-            </SafeAreaView>
-        </ScrollView>
-    );
+            <View style={styles.btnContainer}>
+              <TouchableOpacity
+                style={[styles.btn, { backgroundColor: accentColor }]}
+                onPress={handleImport}
+              >
+                <Text style={styles.text}>Import</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={[styles.btn, { backgroundColor: accentColor }]}
+                onPress={handleExport}
+              >
+                <Text style={styles.text}>Export</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.sectionContainer}>
+            <Text style={[styles.subheading, { color: isDarkMode ? Colors.light : Colors.dark }]}>Report a Bug</Text>
+            <View style={{ flexDirection: 'column', gap: 15 }}>
+              <TouchableOpacity
+                onPress={() => handleLink('https://t.me/+2qtifdFNRv00Y2M9')}
+                style={[styles.btn, { backgroundColor: accentColor }]}
+              >
+                <Text style={styles.text}>Join the Official Telegram Group</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => handleLink('https://github.com/bugsdev2/bookaccio/issues')}
+                style={[styles.btn, { backgroundColor: accentColor }]}
+              >
+                <Text style={styles.text}>Create a New Issue via Github</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </SafeAreaView>
+    </ScrollView>
+  );
 };
 
 export default Settings;
 
 const styles = StyleSheet.create({
-    container: {
-        //
-    },
+  contentContainer: {
+    padding: 10,
+  },
 
-    contentContainer: {
-        padding: 10,
-    },
+  headerContainer: {
+    padding: 10,
+    marginBottom: 10,
+  },
 
-    headerContainer: {
-        padding: 10,
-        marginBottom: 10,
-    },
+  headerTitle: {
+    fontSize: 25,
+    textAlign: 'center',
+    fontFamily: 'MontB',
+  },
 
-    headerTitle: {
-        fontSize: 25,
-        textAlign: 'center',
-        fontFamily: 'MontB',
-    },
+  sectionContainer: {
+    gap: 15,
+  },
 
-    sectionContainer: {
-        gap: 15,
-    },
+  subheading: {
+    textAlign: 'center',
+    fontFamily: 'MontB',
+    fontSize: 18,
+  },
 
-    subheading: {
-        textAlign: 'center',
-        fontFamily: 'MontB',
-        fontSize: 18,
-    },
+  btnContainer: {
+    flexDirection: 'row',
+    gap: 50,
+    margin: 'auto',
+  },
 
-    btnContainer: {
-        flexDirection: 'row',
-        gap: 50,
-        margin: 'auto',
-    },
+  btn: {
+    paddingHorizontal: 25,
+    paddingVertical: 10,
+    borderRadius: 10,
+  },
 
-    btn: {
-        paddingHorizontal: 25,
-        paddingVertical: 10,
-        borderRadius: 10,
-    },
+  modalContainer: {
+    backgroundColor: Colors.light,
+    borderRadius: 20,
+    padding: 25,
+  },
 
-    modalContainer: {
-        backgroundColor: Colors.light,
-        borderRadius: 20,
-        padding: 25,
-    },
-
-    text: {
-        color: Colors.light,
-        fontFamily: 'MontR',
-        textAlign: 'center',
-    },
+  text: {
+    color: Colors.light,
+    fontFamily: 'MontR',
+    textAlign: 'center',
+  },
 });
