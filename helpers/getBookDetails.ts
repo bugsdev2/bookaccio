cb21c8c74f0c380d09167ffc25439fd10d3cc645
Aadi <adithyamanikumar@proton.me>
2024-12-28 01:28:52 +0530

    Added try/catch

--- helpers/getBookDetails.ts
+++ helpers/getBookDetails.ts
@@ -4,6 +4,10 @@ export const getBookDetails = async (bookTitle: string) => {
   if (bookTitle === '') return [];
   const searchTitle = bookTitle.toLowerCase().split(' ').join('+');
   const URL = `https://www.googleapis.com/books/v1/volumes?q=${searchTitle}`;
-  const res = await axios.get(URL);
-  return await res.data.items;
+  try {
+    const res = await axios.get(URL);
+    return await res.data.items;
+  } catch (err) {
+    return undefined;
+  }
 };
