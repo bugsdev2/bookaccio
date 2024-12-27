72b3bdd7709b51a9608478896aec31289baf6979
Aadi <adithyamanikumar@proton.me>
2024-12-28 01:28:19 +0530

    Added the newly added providers to allproviders file

--- providers/providers.tsx
+++ providers/providers.tsx
@@ -3,19 +3,25 @@ import AccentColorProvider from '@/providers/accentColorProvider';
 import SelectedBookProvider from '@/providers/selectedBookProvider';
 import ThemeProvider from '@/providers/themeProvider';
 import FullBooksListProvider from './booksFullListProvider';
+import ShowPageNumberProvider from './options/showPageNumberProvider';
+import ShowRatingProvider from './options/showRatingProvider';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-    return (
+  return (
+    <ShowPageNumberProvider>
+      <ShowRatingProvider>
         <FullBooksListProvider>
-            <SelectedBookProvider>
-                <AccentColorProvider>
-                    <FontsProvider>
-                        <ThemeProvider>{children}</ThemeProvider>
-                    </FontsProvider>
-                </AccentColorProvider>
-            </SelectedBookProvider>
+          <SelectedBookProvider>
+            <AccentColorProvider>
+              <FontsProvider>
+                <ThemeProvider>{children}</ThemeProvider>
+              </FontsProvider>
+            </AccentColorProvider>
+          </SelectedBookProvider>
         </FullBooksListProvider>
-    );
+      </ShowRatingProvider>
+    </ShowPageNumberProvider>
+  );
 };
 
 export default Providers;
