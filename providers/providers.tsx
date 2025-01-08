6e91243acf39c75118aa92edd51972fcead7ace3
Aadi <adithyamanikumar@proton.me>
2025-01-08 18:28:53 +0530

    added the three new providers to the main list of providers

--- providers/providers.tsx
+++ providers/providers.tsx
@@ -5,22 +5,31 @@ import ThemeProvider from '@/providers/themeProvider';
 import FullBooksListProvider from './booksFullListProvider';
 import ShowPageNumberProvider from './options/showPageNumberProvider';
 import ShowRatingProvider from './options/showRatingProvider';
+import BlackThemeProvider from './blackThemeProvider';
+import PreventScreenShotProvider from './options/preventScreenShotProvider';
+import UnfinishedProvider from './options/showUnfinishedProvider';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ShowPageNumberProvider>
-      <ShowRatingProvider>
-        <FullBooksListProvider>
-          <SelectedBookProvider>
-            <AccentColorProvider>
-              <FontsProvider>
-                <ThemeProvider>{children}</ThemeProvider>
-              </FontsProvider>
-            </AccentColorProvider>
-          </SelectedBookProvider>
-        </FullBooksListProvider>
-      </ShowRatingProvider>
-    </ShowPageNumberProvider>
+    <BlackThemeProvider>
+      <ShowPageNumberProvider>
+        <ShowRatingProvider>
+          <FullBooksListProvider>
+            <SelectedBookProvider>
+              <AccentColorProvider>
+                <FontsProvider>
+                  <PreventScreenShotProvider>
+                    <UnfinishedProvider>
+                      <ThemeProvider>{children}</ThemeProvider>
+                    </UnfinishedProvider>
+                  </PreventScreenShotProvider>
+                </FontsProvider>
+              </AccentColorProvider>
+            </SelectedBookProvider>
+          </FullBooksListProvider>
+        </ShowRatingProvider>
+      </ShowPageNumberProvider>
+    </BlackThemeProvider>
   );
 };
 
