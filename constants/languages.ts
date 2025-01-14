1fe1f426bb649010b324d2d3ab03a19662b13d0b
Aadi <adithyamanikumar@proton.me>
2025-01-15 01:11:46 +0530

    added language lists

---
+++ constants/languages.ts
@@ -0,0 +1,6 @@
+import { languageList } from './languageList';
+
+export const languages = [
+  { title: languageList['en'].nativeName, value: 'en' },
+  { title: languageList['it'].nativeName, value: 'it' },
+];
