a912f9b5c503260ed06677a5bf80e3abf772f8b6
Aadi <adithyamanikumar@proton.me>
2025-01-15 01:29:16 +0530

    spacing autofix

--- helpers/getBookList.ts
+++ helpers/getBookList.ts
@@ -1,14 +1,14 @@
 import { getData } from './storage';
 
 export const getBookList = async (): Promise<Book[] | any> => {
-    try {
-        const data: Book[] = await getData('bookList');
-        if (data) {
-            return await data;
-        } else {
-            return [];
-        }
-    } catch (err: any) {
-        return err;
+  try {
+    const data: Book[] = await getData('bookList');
+    if (data) {
+      return await data;
+    } else {
+      return [];
     }
+  } catch (err: any) {
+    return err;
+  }
 };
