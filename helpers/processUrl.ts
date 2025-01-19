93207b27dc86298bc4bc987f19e55b5502b89e69
Aadi <adithyamanikumar@proton.me>
2025-01-19 19:54:49 +0530

    Commented out the openlibrary cover modifier since I'm not using openlibrary api at the moment

--- helpers/processUrl.ts
+++ helpers/processUrl.ts
@@ -2,7 +2,7 @@ export const processUrl = (url: string | undefined) => {
   let newUrl;
   if (url) {
     newUrl = url.replace('http:', 'https:');
-    newUrl = newUrl.replace('-S.jpg', '-L.jpg');
+    // newUrl = newUrl.replace('-S.jpg', '-L.jpg');
   }
   return newUrl;
 };
