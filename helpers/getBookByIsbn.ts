a0f7c2c93127a74b74e785db0f7e9010f0e37c09
Aadi <adithyamanikumar@proton.me>
2025-01-19 19:55:53 +0530

    Added type for better handling

--- helpers/getBookByIsbn.ts
+++ helpers/getBookByIsbn.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-export async function getBookByIsbn(value: string) {
+export async function getBookByIsbn(value: string): Promise<undefined | BookSearchResultProp> {
   try {
     const res = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=isbn:${value}`);
     const selected: BookSearchResultProp = res.data?.items[0];
-    const response = (await axios.get(selected.selfLink)).data;
+    const response: BookSearchResultProp = (await axios.get(selected.selfLink)).data;
     return response;
   } catch (err) {
     return undefined;
