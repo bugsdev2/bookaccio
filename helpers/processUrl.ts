fe99b17fb2683ee7b0935a736565b572f7483543
Aadi <adithyamanikumar@proton.me>
2025-01-19 19:54:01 +0530

    added a replace method to replace image -S to -L for large images (for open-library covers)

--- helpers/processUrl.ts
+++ helpers/processUrl.ts
@@ -1,7 +1,8 @@
 export const processUrl = (url: string | undefined) => {
-    let newUrl;
-    if (url) {
-        newUrl = url.replace('http:', 'https:');
-    }
-    return newUrl;
+  let newUrl;
+  if (url) {
+    newUrl = url.replace('http:', 'https:');
+    newUrl = newUrl.replace('-S.jpg', '-L.jpg');
+  }
+  return newUrl;
 };
